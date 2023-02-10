6377e465f2450b8c0fbe4703c59272b326e4482c
subeksharma <subeksharmaofficial@gmail.com>
2023-02-10 17:42:01 +0545

    some changes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router()
 const bookTicketRoute = require("./bookTicketRoute")
+const updateTicketRoute = require("./updateTicketRoute")
+const deleteTicketRoute = require("./deleteTicketRoute")
+
 
 router.use("/book", bookTicketRoute)
+router.use("/update", updateTicketRoute)
+router.use("/delete", deleteTicketRoute)
+
 
 module.exports = router
