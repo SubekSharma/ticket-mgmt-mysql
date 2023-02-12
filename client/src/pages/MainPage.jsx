0bdba89dd5fe5ddbb6cac3292c600aa228eb14be
subeksharma <subeksharmaofficial@gmail.com>
2023-02-12 18:39:07 +0545

    some changes

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -6,12 +6,13 @@ import BookTicketForm from "../components/BookTicketForm";
 export default function MainPage() {
   return (
     <div className="container">
+      <Header />
       <div className="d-flex justify-content-between">
         <div
           className="w-25 my-4 position-fixed"
           style={{ marginRight: "50%" }}
         >
-          <Header />
+          
           <BookTicketForm />
         </div>
         <div className="w-50 my-4" style={{ marginLeft: "50%" }}>
